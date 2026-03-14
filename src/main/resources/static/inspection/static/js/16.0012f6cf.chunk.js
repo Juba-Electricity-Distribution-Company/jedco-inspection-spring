3d3e4d5d6f34128e5ff753e6ff94bae35ca233ed
Mirtineh <mirfil92@gmail.com>
2026-03-14 09:33:56 +0200

    add static files

---
+++ src/main/resources/static/inspection/static/js/16.0012f6cf.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[16],{881:function(e,t,c){"use strict";c.r(t);c(0);var i=c(569),s=(c(571),c(10));t.default=()=>Object(s.jsxs)(i.Q,{children:[Object(s.jsx)(i.i,{sm:"6",lg:"3"}),Object(s.jsx)(i.i,{sm:"6",lg:"3"})]})}}]);
+//# sourceMappingURL=16.0012f6cf.chunk.js.map
